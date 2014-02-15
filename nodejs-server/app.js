dfed0267816cb5087fa25962be66af04fcd2d12b
wenren <xiangwenwe@foxmail.com>
2014-02-15 20:59:07 +0800

    delete

--- nodejs-server/app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var  app = express()
-app.get('/zip',function(request,respones){
-      var xml = '<?xml version="1.0" encoding="utf-8"?> <zip>https://github.com/xiangwenwen/downloadZIP/archive/master.zip</zip>';
-      respones.type('xml');
-      respones.send(xml) 
-});
-app.listen(9898)  
-console.log('server start 127.0.0.1L:9898 for xml Data');
