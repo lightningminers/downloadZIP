94bc741839b32dc9d4e053573230af23f7538852
wenren <xiangwenwe@foxmail.com>
2014-02-21 19:29:49 +0800

    install python modules

--- install.py
+++ install.py
@@ -6,15 +6,18 @@ import zipfile
 #https://pypi.python.org/packages/source/s/suds-jurko/suds-jurko-0.4.1.jurko.3.zip#md5=921205d8f9d3f8db2ecb9bae5cd9411a
 
 def sudsInstall():
-    if not os.path.isdir(os.getcwd() + os.path.sep + 'sudsinstall'):
+    if not os.path.isdir(os.getcwd() + os.path.sep + 'suds-jurko-0.4.1.jurko.3'):
         sudsSetup = os.getcwd() + os.path.sep + 'suds-jurko-0.4.1.jurko.3.zip'
+        msnlibSetup = os.getcwd() + os.path.sep + 'msnlib.zip'
         zip_suds = zipfile.ZipFile(sudsSetup,'r')
         zip_suds.extractall(path=os.getcwd())
-        os.rename(os.getcwd() + os.path.sep + 'suds-jurko-0.4.1.jurko.3',os.getcwd() + os.path.sep + 'sudsinstall')
-    import sudsinstall.setup
-    sudsinstall.setup.setup()
+        zip_suds.close()
+        zip_msnlib = zipfile.ZipFile(msnlibSetup,'r')
+        zip_msnlib.extractall(path=os.getcwd())
+        zip_msnlib.close()
+        return True
+    return False
     pass
-sudsInstall()
 
 
 
