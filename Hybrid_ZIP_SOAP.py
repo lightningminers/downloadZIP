42be1de4fdf6ffba50276c101d1e8cc537f0e3bb
wenren <xiangwenwe@foxmail.com>
2014-03-08 15:54:35 +0800

    增加线程在后台处理下载，解决下载时会卡界面的问题

--- Hybrid_ZIP_SOAP.py
+++ Hybrid_ZIP_SOAP.py
@@ -12,21 +12,13 @@ import xml.dom.minidom
 import shutil
 import zipfile
 import time
-import subprocess
 import suds.client
-# import install
-# modubool = install.sudsInstall()
+import threading
+import traceback
+import queue
 
-# print(modubool)
-# subprocess.call('python suds-jurko-0.4.1.jurko.3/setup.py install',shell=True)
-# subprocess.call('python msnlib/setup.py install',shell=True)
-# if modubool:
-#     subprocess.call('python ez_setup.py install',shell=True)
-#     subprocess.call('python suds-jurko-0.4.1.jurko.3/setup.py install',shell=True)
 
-# subprocess.call('python msnlib/setup.py installl',shell=True)
-# import msnlib
-
+# 临时解决依赖模块问题，加入系统模块路径
 # sys.path.append("./suds-jurko-0.4.1.jurko.3")
 # import suds
 
@@ -50,15 +42,16 @@ G_LOG = []
 workPath = os.getcwd()
 downloadPath = workPath + os.path.sep + 'download'
 webappmkdir = downloadPath + os.path.sep + 'webapp'
-webLog = downloadPath + os.path.sep + 'webLog'
+errorLog = downloadPath + os.path.sep + 'errorLog'
+error_log = errorLog + os.path.sep + 'error.txt'
 if os.path.isdir(downloadPath):
     shutil.rmtree(downloadPath)
 os.mkdir(downloadPath)
 os.mkdir(webappmkdir)
-os.mkdir(webLog)
+os.mkdir(errorLog)
 G_LOG.append('mkdir:'+downloadPath + '\n')
 G_LOG.append('mkdir:'+webappmkdir + '\n')
-G_LOG.append('mkdir:'+webLog + '\n')
+G_LOG.append('mkdir:'+errorLog + '\n')
 
 #获取当前系统时间
 theTime = time.strftime('%Y-%m-%d-%H-%M',time.localtime(time.time()))
@@ -88,155 +81,71 @@ def getText(nodelist):
         if node.nodeType == node.TEXT_NODE:
             rc.append(node.data)
     return ''.join(rc)
-# 解压
-def unzip_file(zipfilename, unziptodir):
-    if not os.path.exists(unziptodir): os.mkdir(unziptodir)
-    zfobj = zipfile.ZipFile(zipfilename,'r')
-    for name in zfobj.namelist():
-        name = name.replace('\\','/')
-        if name.endswith('/'):
-            os.mkdir(os.path.join(unziptodir, name))
-        else:
-            ext_filename = os.path.join(unziptodir, name)
-            ext_dir= os.path.dirname(ext_filename)
-            if not os.path.exists(ext_dir) : os.mkdir(ext_dir)
-            outfile = open(ext_filename, 'wb')
-            outfile.write(zfobj.read(name))
-            outfile.close()
     pass
-
+#在后台处理下载
+class StageHandler(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.source = None
+        pass
+    def run(self):
+        #显示下载进度
+        def downSize(count, blockSize, totalSize):
+            if not count:
+                print('connection opened')
+            if totalSize <0:
+                print('远程主机，返回的下载总量为负数')
+                print('read %d blocks' % count)
+            else:
+                print ('download '+zip_down[1]+' %d KB, totalsize: %d KB' % (count*blockSize/1024.0,totalSize/1024.0))
+            pass
+        #下载zip文件并解压缩至webapp
+        zip_all = []
+        for zip_down in self.source:
+            #远程主机，返回的头信息是否与此匹配，来判断是否是zip文件 application/x-zip-compressed
+            remoteHostTest = urllib.request.urlopen(zip_down[1])
+            if not remoteHostTest.headers['Content-Type'] == 'application/x-zip-compressed':
+                continue
+            print('request '+ zip_down[1] +' wait ....')
+            G_LOG.append('DOWNLOAD '+ zip_down[1] +'\n')
+            try:
+                zip_add = downloadPath+os.path.sep+zip_down[0]+'.zip'
+                zip_all.append(zip_add)
+                urllib.request.urlretrieve(zip_down[1],zip_add,reporthook=downSize)
+            except ValueError:
+                print(zip_down[1])
+                raise ValueError('Error: download url bad'+zip_down[1])
+                G_LOG.append('Error: download url bad'+zip_down[1]+'\n')
+        print('download is success')
+        for zip_app in zip_all:
+            zip_file = zipfile.ZipFile(zip_app,'r')
+            zip_name_file = zip_file.namelist()
+            zip_info_file = zip_file.infolist()
+            for name in zip_name_file:
+                G_LOG.append('UN ZIP '+name + '\n')
+            zip_file.extractall(path = webappmkdir)
+            zip_file.close()
+            pass
+        pass
+    pass
 #主类
 class ClientApp:
-    def __init__(self,width=600,height=150):
+    def __init__(self,width=600,height=150,stage=None):
         self.root = tkinter.Tk()
         self.root.geometry('{0}x{1}'.format(width,height))
         self.root.title('downloadZIP')
         label = tkinter.Label(self.root,text='download zip the current directory mkdir webapp and download')
         label.pack(padx=5,pady=5,anchor='w')
+        self.stage = stage
         pass
-    def downloadControls(self):
-        #打包webpp
-        def unpackwebapp(webappmkdir):
-            print('unpack webapp path :'+ webappmkdir)
-            G_LOG.append('unpack webapp path :'+ webappmkdir+'\n')
-            unpack_zip = zipfile.ZipFile('webapp.zip','w')
-            for dirpath, dirnames, filenames in os.walk(webappmkdir):
-                for filename in filenames:
-                    unpack_zip_path = os.path.join('webapp'+os.path.sep+ dirpath.split('webapp')[1],filename)
-                    print('wait :' + filename)
-                    unpack_zip.write(os.path.join(dirpath,filename),unpack_zip_path)
-                    print('success :' + filename)
-                    G_LOG.append('SUCCESS ' + unpack_zip_path + '\n')
-            print('done')
-            unpack_zip.close()
-            webLogFile = open(webLog+os.path.sep+'web-log-'+theTime+'.txt','w',encoding='utf-8')
-            webLogFile.write(' '.join(G_LOG))
-            webLogFile.close()
-            tkinter.messagebox.showinfo('SUCCESS MESSAGE','DONE')
-            pass
-        #检校md5
-        def calibrationMD5():
-
-            pass
-        #下载zip文件并解压缩至webapp
-        def zipDecompression(downloadStart):
-            #显示下载进度
-            def downSize(count, blockSize, totalSize):
-                if not count:
-                    print('connection opened')
-                if totalSize <0:
-                    print('远程主机，返回的下载总量为负数')
-                    print('read %d blocks' % count)
-                else:
-                    print ('download '+zip_down[1]+' %d KB, totalsize: %d KB' % (count*blockSize/1024.0,totalSize/1024.0))
-                pass
-            zip_all = []
-            for zip_down in downloadStart:
-                #远程主机，返回的头信息是否与此匹配，来判断是否是zip文件 application/x-zip-compressed
-                remoteHostTest = urllib.request.urlopen(zip_down[1])
-                if not remoteHostTest.headers['Content-Type'] == 'application/x-zip-compressed':
-                    continue
-                print('request '+ zip_down[1] +' wait ....')
-                G_LOG.append('DOWNLOAD '+ zip_down[1] +'\n')
-                try:
-                    zip_add = downloadPath+os.path.sep+zip_down[0]+'.zip'
-                    zip_all.append(zip_add)
-                    urllib.request.urlretrieve(zip_down[1],zip_add,reporthook=downSize)
-                except ValueError:
-                    print(zip_down[1])
-                    raise ValueError('Error: download url bad'+zip_down[1])
-                    G_LOG.append('Error: download url bad'+zip_down[1]+'\n')
-
-            print('download is success')
-            for zip_app in zip_all:
-                zip_file = zipfile.ZipFile(zip_app,'r')
-                zip_name_file = zip_file.namelist()
-                zip_info_file = zip_file.infolist()
-                for name in zip_name_file:
-                    G_LOG.append('UN ZIP '+name + '\n')
-                zip_file.extractall(path = webappmkdir)
-                zip_file.close()
-                pass
-            unpackwebapp(webappmkdir)
-            pass
-        #解析 XML
-        def handlerparseXML(xmlData):
-            SOAP_ZIP_XML_PATH = downloadPath + os.path.sep +'SOAP_zip.xml'
-            with open(SOAP_ZIP_XML_PATH,'w',encoding='utf-8') as xml_f:
-                xml_f.write(xmlData)
-                xml_f.close()
-            SOAP_DIC = {}
-            SOAP_XML_DOM = xml.dom.minidom.parseString(xmlData)
-            SOAP_XMl_HEAD = SOAP_XML_DOM.getElementsByTagName('Header')
-            SOAP_DIC['ServerIP'] = SOAP_XMl_HEAD[0].getAttribute('ServerIP')
-            SOAP_DIC['ResultCode'] = SOAP_XMl_HEAD[0].getAttribute('ResultCode')
-            SOAP_DIC['ResultMsg'] = SOAP_XMl_HEAD[0].getAttribute('ResultMsg')
-            SOAP_DIC['ResultNo'] = SOAP_XMl_HEAD[0].getAttribute('ResultNo')
-            if not len(SOAP_DIC['ServerIP']):
-                print('ERROR: SOAP REQUEST BAD')
-                print(SOAP_DIC['ResultNo'])
-                print(SOAP_DIC['ResultMsg'])
-                G_LOG.append('ERROR: SOAP REQUEST BAD \n')
-                G_LOG.append('THE ERRPR MESSAGE '+SOAP_DIC['ResultMsg']+'\n')
-                return
-            SOAP_XML_Result = SOAP_XML_DOM.getElementsByTagName('Result')
-            # print(getText(SOAP_XML_Result[0].childNodes))
-            # SOAP_XML_Result_Text = SOAP_XML_Result[0].data()
-            # print(SOAP_XML_Result_Text)
-            SOAP_XML_HybridPackage = SOAP_XML_DOM.getElementsByTagName('HybridPackage')
-            downloadALL = []
-            count = -1
-            for node in SOAP_XML_HybridPackage:
-                downloadALL.append([])
-                count += 1
-                for node_value in node.childNodes:
-                    node_text = getText(node_value.childNodes)
-                    if len(node_text):
-                        downloadALL[count].append(node_text)
-            zipDecompression(downloadALL)
-            pass
-        #SOAP WEBSERVICE
-        def getValue(event):
-            SOAPURL = 'http://wb.mobile.sh.ctripcorp.com/hybridpublish/service.asmx?wsdl'
-            SOAPSENDDATA = getVersion.get()
-            SOAPSENDDATA = bodyXML.format(SOAPSENDDATA)
-            SOAPSENDDATA = formatXML(SOAPSENDDATA)
-            try:
-                webservice = suds.client.Client(SOAPURL)
-                SOAPRESPONSE = webservice.service.Request(SOAPSENDDATA)
-                print('SOAP SUCCESS:'+SOAPURL)
-                G_LOG.append('SOAP URL :' +SOAPURL + '\n')
-                G_LOG.append('SOAP SEND DATA :' +SOAPSENDDATA + '\n' )
-            except ValueError:
-                raise  ValueError ('url bad')
-            handlerparseXML(SOAPRESPONSE)
-            pass
+    #渲染UI界面
+    def renderui(self):
         messURL = tkinter.Label(self.root,text='SOAP URL: http://wb.mobile.sh.ctripcorp.com/hybridpublish/service.asmx?wsdl')
         messURL.pack(padx=5,anchor='w')
-        parmas = tkinter.Label(self.root,text='Send VERSION:')
+        parmas = tkinter.Label(self.root,text='send SOAP version:')
         parmas.pack(padx=5,anchor='w')
-        getVersion = tkinter.Entry(self.root,width=500)
-        getVersion.pack(padx=5,pady=5)
+        self.getVersion = tkinter.Entry(self.root,width=500)
+        self.getVersion.pack(padx=5,pady=5)
         # getSendDate = tkinter.Text(self.root,width=400,height=10)
         # getSendDate.pack(padx=5,pady=5,anchor='w')
         # getSendDate.focus_set()
@@ -244,16 +153,95 @@ class ClientApp:
         # Tscrollbar.pack(side='right',fill='y')
         # Tscrollbar.config(command=getSendDate.yview)
         # getSendDate.config(yscrollcommand=Tscrollbar.set)
-        openSOAP = tkinter.Button(self.root,text='openSOAP and unpackWebapp',width=30,height=1)
-        openSOAP.bind('<Button-1>',getValue)
+        openSOAP = tkinter.Button(self.root,text='exec',width=30,height=1)
+        openSOAP.bind('<Button-1>',self.sendRquest)
         openSOAP.pack(padx=5,pady=5,anchor='w')
         pass
+    #打包webpp
+    def unpackwebapp(self):
+        print('unpack webapp path :'+ webappmkdir)
+        G_LOG.append('unpack webapp path :'+ webappmkdir+'\n')
+        unpack_zip = zipfile.ZipFile('webapp.zip','w')
+        for dirpath, dirnames, filenames in os.walk(webappmkdir):
+            for filename in filenames:
+                unpack_zip_path = os.path.join('webapp'+os.path.sep+ dirpath.split('webapp')[1],filename)
+                print('wait :' + filename)
+                unpack_zip.write(os.path.join(dirpath,filename),unpack_zip_path)
+                print('success :' + filename)
+                G_LOG.append('SUCCESS ' + unpack_zip_path + '\n')
+        print('done')
+        unpack_zip.close()
+        webLogFile = open(errorLog+os.path.sep+'web-log-'+theTime+'.txt','w',encoding='utf-8')
+        webLogFile.write(' '.join(G_LOG))
+        webLogFile.close()
+        tkinter.messagebox.showinfo('SUCCESS MESSAGE','DONE')
+        pass
+    #检校md5
+    def calibrationMD5(self):
+
+        pass
+    #解析 XML
+    def handlerparseXML(self,xmlData):
+        SOAP_ZIP_XML_PATH = downloadPath + os.path.sep +'SOAP_zip.xml'
+        with open(SOAP_ZIP_XML_PATH,'w',encoding='utf-8') as xml_f:
+            xml_f.write(xmlData)
+            xml_f.close()
+        SOAP_DIC = {}
+        SOAP_XML_DOM = xml.dom.minidom.parseString(xmlData)
+        SOAP_XMl_HEAD = SOAP_XML_DOM.getElementsByTagName('Header')
+        SOAP_DIC['ServerIP'] = SOAP_XMl_HEAD[0].getAttribute('ServerIP')
+        SOAP_DIC['ResultCode'] = SOAP_XMl_HEAD[0].getAttribute('ResultCode')
+        SOAP_DIC['ResultMsg'] = SOAP_XMl_HEAD[0].getAttribute('ResultMsg')
+        SOAP_DIC['ResultNo'] = SOAP_XMl_HEAD[0].getAttribute('ResultNo')
+        if not len(SOAP_DIC['ServerIP']):
+            print('ERROR: SOAP REQUEST BAD')
+            print(SOAP_DIC['ResultNo'])
+            print(SOAP_DIC['ResultMsg'])
+            G_LOG.append('ERROR: SOAP REQUEST BAD \n')
+            G_LOG.append('THE ERRPR MESSAGE '+SOAP_DIC['ResultMsg']+'\n')
+            return
+        SOAP_XML_Result = SOAP_XML_DOM.getElementsByTagName('Result')
+        SOAP_XML_HybridPackage = SOAP_XML_DOM.getElementsByTagName('HybridPackage')
+        downloadALL = []
+        count = -1
+        for node in SOAP_XML_HybridPackage:
+            downloadALL.append([])
+            count += 1
+            for node_value in node.childNodes:
+                node_text = getText(node_value.childNodes)
+                if len(node_text):
+                    downloadALL[count].append(node_text)
+        self.stage.source = downloadALL
+        pass
+    #向远程SOAP webservice 发起请求
+    def sendRquest(self,event):
+        SOAPURL = 'http://wb.mobile.sh.ctripcorp.com/hybridpublish/service.asmx?wsdl'
+        SOAPSENDDATA = self.getVersion.get()
+        SOAPSENDDATA = bodyXML.format(SOAPSENDDATA)
+        SOAPSENDDATA = formatXML(SOAPSENDDATA)
+        try:
+            webservice = suds.client.Client(SOAPURL)
+            SOAPRESPONSE = webservice.service.Request(SOAPSENDDATA)
+            print('SOAP SUCCESS:'+SOAPURL)
+            G_LOG.append('SOAP URL :' +SOAPURL + '\n')
+            G_LOG.append('SOAP SEND DATA :' +SOAPSENDDATA + '\n' )
+        except urllib.error.URLError:
+            self.error_outinput()
+            print('SOAP REQUEST BAD')
+        pass
+    #循环UI
     def loop(self):
         self.root.resizable(False,False)
-        self.downloadControls()
+        self.renderui()
         self.root.mainloop()
         pass
+    #错误信息输出
+    def error_outinput(self):
+        error_f = open(error_log,'a')
+        traceback.print_exc(file=error_f)
+        error_f.close()
+        pass
     pass
-
-app = ClientApp()
+stage = StageHandler()
+app = ClientApp(stage=stage)
 app.loop()
